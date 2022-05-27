ad3c73b1873b229fb21e861a04b3056abed46d7c
Hattsu <102260446+Hattsu@users.noreply.github.com>
2022-05-26 22:42:34 -0500

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ByronCalderon3A_Ejercicio8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Multiplos muktiplos = new Multiploa();
+            multiplos.Calcular_multiplo();
+        }
+    }
+}
